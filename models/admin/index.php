c813186bfea736fb0ff2689c20907669bc1db4fb
Brunno hofmann <brunnohofmann@hotmail.com>
2015-04-16 12:39:48 -0300

    Ajustes de visual

--- models/admin/index.php
+++ models/admin/index.php
@@ -33,7 +33,7 @@
     <!--top menu-->
     <div class="row">
         <div class="col-md-12">
-            <nav class="navbar navbar-default" role="navigation">
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="z-index: 10">
                 <div class="container-fluid">
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -42,15 +42,18 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="#"><strong>DOM</strong></a>
+                        <a class="navbar-brand nopadding" href="#"><img src="models/includes/img/marca.png" style="width: 40%" alt=""/></a>
                     </div>
+
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                       <form class="navbar-form navbar-right" role="search">
+                        <p class="navbar-text navbar-right"><span class="text-success">Gerenciador de Conteúdo</span>Usuário logado - <a class="navbar-link" href=""><b>Sair</b></a></p>
+
+                      <!-- <form class="navbar-form navbar-right" role="search">
                             <div class="form-search search-only">
                                 <i class="search-icon glyphicon glyphicon-search"></i>
                                 <input class="form-control search-query">
                             </div>
-                        </form>
+                        </form> -->
                     </div>
                 </div>
             </nav>
@@ -113,14 +116,14 @@
     			<span class="hamb-middle"></span>
 				<span class="hamb-bottom"></span>
             </button>
-            <div class="container">
+
                 <div class="row">
-                    <div class="col-lg-8 col-lg-offset-2 pages">
+                    <div class="col-md-12 pages">
 
 
                     </div>
                 </div>
-            </div>
+
         </div>
         <!-- /#page-content-wrapper -->
 
