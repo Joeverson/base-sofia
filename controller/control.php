8c321fb54e9016f8342855beeac7cdbe1606238c
Brunno Hofmann <brunnohofmann@hotmail.com>
2015-05-04 15:01:17 -0300

    Ajustes em toda a estrutura. O objetivo da versão é prover um CMS base para qualquer projeto de site. Foi retirada a opção que inseria todas as página por ajax.

--- controller/control.php
+++ controller/control.php
@@ -30,7 +30,7 @@
          require 'Slim/Slim.php';
          \Slim\Slim::registerAutoloader();
 
-         $app = new \Slim\Slim(array('templates.path' => '/home/Windows/Users/Brunno Hofmann/Copy/Sites Backup/cms/models')); // retorna a instancia
+         $app = new \Slim\Slim(array('templates.path' => 'c:\xampp\htdocs\cms\models')); // retorna a instancia
 
          $app->add(new \Slim\Middleware\SessionCookie(array(
              'expires' => '20 minutes',
