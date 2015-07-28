ce9c1cb90db9b532920e5aad66bb941986c1324a
ghuther <joerverson.santos@gmail.com>
2015-07-28 20:16:20 -0300

    assing mine

--- models/admin/slider/pages/delete.php
+++
@@ -1,2 +0,0 @@
-<p class="urlTrasiction" data-url="../models/admin/slider/controller/delete.php">Deseja deletar o slider?</p>
-<input type="hidden" name="id_slider" value="<?=$_POST['id']?>"/>
--- models/admin/vereadores/pages/delete.php
+++
@@ -1,2 +0,0 @@
-<p class="urlTrasiction" data-url="../models/admin/vereadores/controller/delete.php">Deseja mesmo deletar?</p>
-<input type="hidden" name="id_vereador" value="<?=$_POST['id']?>"/>
